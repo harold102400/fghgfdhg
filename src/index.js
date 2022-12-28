82c8a170cc21452489c7d5541cbe05a69bf57cfe
Harold Montes <haroldjosehd24@gmail.com>
2022-12-28 12:38:58 -0500

    add delete button for local page and api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-const form = document.querySelector('.form');
-form.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    addNewTaskToServer()
-    form.reset()
-})
+const form = document.querySelector(".form");
 const todoTableBody = document.querySelector(".todo__table__body");
 let allTodosTasks = [];
 
 ///se hace una peticion HTML
 const getTodoTasks = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await response.json();
   //console.log(data);
   return data;
@@ -19,27 +14,29 @@ getTodoTasks().then((data) => setTasksToArray(data));
 //pasa los task a el array[]
 const setTasksToArray = (task) => {
   allTodosTasks = task;
-  setTasks(allTodosTasks);
+  renderAllTasks(allTodosTasks);
   //console.log(allTodosTasks);
 };
 //se usa el forEach para buscar dentro del array del json y se usa la funcion render para pasarle la data
-const setTasks = (tasks) => {
+const renderAllTasks = (tasks) => {
+  todoTableBody.innerHTML = '';
   tasks.forEach((task, index) => {
-    renderTodos(task, index);
+    renderTodo(task, index);
   });
 };
 //se crea una funcion render que crea las tareas
-const renderTodos = (task, index) => {
+const renderTodo = (task, index) => {
+  const br = document.createElement("br");
   const tr = document.createElement("tr");
 
   const tdID = document.createElement("td");
   tdID.textContent = task.id;
 
   const tdTitle = document.createElement("td");
-  tdTitle.textContent = task.title;
+  tdTitle.textContent = task.name;
 
   const tdDescription = document.createElement("td");
-  tdDescription.textContent = task.body;
+  tdDescription.textContent = task.username;
 
   const tdStatus = document.createElement("td");
   tdStatus.textContent = "pending";
@@ -49,19 +46,20 @@ const renderTodos = (task, index) => {
   editButton.textContent = "Edit";
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "Delete";
-  deleteButton.addEventListener('click', ()=>{
-    removeFromApi(index)
-  })
+  deleteButton.addEventListener("click", () => {
+    removeFromApi(task, index);
+  });
 
   tdEditAndDelete.append(editButton, deleteButton);
   tr.append(tdID, tdTitle, tdDescription, tdStatus, tdEditAndDelete);
   todoTableBody.append(tr);
+  todoTableBody.append(br);
   return tr;
 };
 
 const addNewTask = (newtask) => {
   allTodosTasks.push(newtask);
-  renderTodos(allTodosTasks);
+  renderTodo(allTodosTasks);
 };
 
 const addNewTaskToServer = async (task) => {
@@ -73,27 +71,27 @@ const addNewTaskToServer = async (task) => {
     body: JSON.stringify(task),
   });
   const data = await response.json();
-  addNewTask(data)
+  addNewTask(data);
 };
 
-const removeCurrentTask = (index) =>{
+const removeCurrentTask = (index) => {
   /* const removeTask = allTodosTasks.filter((value) =>{
     console.log('This is the value' + JSON.stringify(value.id))
     const Trueorfalse = value.id !== id
     return Trueorfalse;
   }) */
-  const removeTask = allTodosTasks.splice(index, 1)
-  //console.log(removeTask)
-  setTasks(removeTask);
-}
+  console.log(index)
+  allTodosTasks.splice(index, 1);
+  console.log(index)
+  renderAllTasks(allTodosTasks);
+};
 
-const removeFromApi = async (id) => {
-   await fetch(`https://jsonplaceholder.typicode.com/posts${id}`, {
+const removeFromApi = async (id ,index) => {
+  await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
     method: "DELETE",
   });
-  removeCurrentTask(id)
-}
-
+  removeCurrentTask(index);
+};
 
 /* let newArray = ['hola', 'adios', 'buenas']
 let results = newArray.splice(2, 1)
